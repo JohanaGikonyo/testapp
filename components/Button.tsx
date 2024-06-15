3a9bdcb49cf659088103046606e27c4d0d7f0832
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-06-15 11:42:23 +0300

    import

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import ButtonProps from "../types";
+import ButtonProps from "../types/index.ts";
 const ButtonComponent = ({ label, icon, colors }: ButtonProps) => {
   return (
     <div className={` rounded rounded-4xl flex gap-2 cursor-pointer text-center items-center`}>
