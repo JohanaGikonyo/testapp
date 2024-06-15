11946122d4e543d2ec82646c0921c26979e2b66b
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-06-15 11:31:02 +0300

    removed extension

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Welcome from "../components/Welcome";
 import Slider from "../components/Slider";
 import Guide from "../components/Guide.jsx";
-import Features from "../components/Features.tsx";
+import Features from "../components/Features";
 import Get from "../components/Get.jsx";
 export default function Home() {
   return (
